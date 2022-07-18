20539fd96bca0a327bc853178ebf6dbbe983d4bd
Raj Shrivastva <rajshrivastva1357@gmail.com>
2022-07-19 00:19:19 +0530

    first commit

---
+++ WebApplication/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebApplication;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DemoClass obj = new DemoClass();
+            Assert.AreEqual("Raj", obj.fname);
+            Assert.AreEqual("Shrivastva", obj.lname);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/DemoClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication
+{
+    public class DemoClass
+    {
+        public string fname;
+        public string lname;
+        public DemoClass()
+        {
+            this.fname = "Raj";
+            this.lname = "Shrivastva";
+        }
+    }
+}
